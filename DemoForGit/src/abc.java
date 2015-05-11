ce3123c5d62aa90848d37aaba06a6e22f8fdeda5
Shwetanshu Sinha <shwetanshu_sinha@infosys.com>
2015-05-11 15:07:26 +0530

    My First Commit !!

---
+++ DemoForGit/src/abc.java
@@ -0,0 +1,36 @@
+
+public class abc {
+	String a;
+	String b;
+	String c;
+	
+	public abc(String a, String b, String c){
+		
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		
+	}
+	
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+	public String getC() {
+		return c;
+	}
+	public void setC(String c) {
+		this.c = c;
+	}
+	
+	
+
+}
---
+++ DemoForGit/src/xyz.java
@@ -0,0 +1,16 @@
+
+public class xyz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		abc Yo = new abc("Good", "Bad", "Ugly");
+		
+		System.out.println("The "+ Yo.getA()+"  The "+ Yo.getB()+"   And The "+Yo.getC());
+		
+		System.out.println("----------------------------------Is One Hell of a movie!!!");
+		
+		System.out.println("Yo!! Change Ready To Test Commit Action");
+	}
+
+}
